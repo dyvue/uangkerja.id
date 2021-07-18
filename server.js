889424bc01807d3ea5b92ddb34ba455fcf3e1b45
Romadhan Prasetyo <romadhanedy@gmail.com>
2021-07-18 21:37:34 +0700

    update(app): env port

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ io.on('connection', socket => {
 
 app.use(express.static(__dirname + '/public'))
 
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
